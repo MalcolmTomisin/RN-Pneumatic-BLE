1a5a83302dd467100444de44f76e8508749eda3b
Tomisin Alufogejo <malcolmtomisin@gmail.com>
2022-12-06 00:28:19 +0100

    add babel dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,40 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+// module.exports = {
+//   presets: ['module:metro-react-native-babel-preset'],
+//   env: {
+//     production: {
+//       plugins: ['react-native-paper/babel'],
+//     },
+//   },
+// };
+
+module.exports = api => {
+  const babelEnv = api.env();
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.android.js',
+          '.android.tsx',
+          '.ios.js',
+          '.ios.tsx',
+        ],
+        root: ['.'],
+      },
+    ],
+    // ['module:react-native-dotenv'],
+    // ['react-native-paper/babel'],
+  ];
+  //change to 'production' to check if this is working in 'development' mode
+  if (babelEnv !== 'development') {
+    plugins.push(['transform-remove-console', {exclude: ['error', 'warn']}]);
+  }
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [...plugins, 'react-native-reanimated/plugin'],
+  };
 };
