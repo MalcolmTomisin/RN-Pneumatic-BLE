aaf1c38d37347f326004bb4c8a0c0d6ba31bd945
Tomisin Alufogejo <malcolmtomisin@gmail.com>
2022-12-06 01:11:46 +0100

    configure react native paper UI library

--- App.tsx
+++ App.tsx
@@ -12,16 +12,23 @@ import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from 'src/navigators';
 import SplashScreen from 'react-native-splash-screen';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {useColorScheme} from 'react-native';
+import {CombinedDefaultTheme, CombinedDarkTheme} from 'src/config';
 
 const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const theme = isDarkMode ? CombinedDarkTheme : CombinedDefaultTheme;
   useEffect(() => {
     SplashScreen.hide();
   }, []);
 
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 };
 
