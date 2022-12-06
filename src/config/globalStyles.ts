6f5d1910a6f6cc563d1b843960011cec32c32008
Tomisin Alufogejo <malcolmtomisin@gmail.com>
2022-12-06 01:12:07 +0100

    add globalstyles

---
+++ src/config/globalStyles.ts
@@ -0,0 +1,63 @@
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+} from 'react-native-paper';
+import {Dimensions} from 'react-native';
+
+const screen_width = Dimensions.get('window').width;
+
+const theme = {
+  ...PaperDefaultTheme,
+  roundness: (8 / 375) * screen_width,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    primary: '#0078C1',
+    accent: '#F3F9FD',
+    primaryInactive: '#FCB195',
+    background: '#F3F9FD',
+  },
+};
+
+const darkTheme = {
+  ...PaperDarkTheme,
+  roundness: (8 / 375) * screen_width,
+  colors: {
+    ...PaperDarkTheme.colors,
+    primary: '#0078C1',
+    accent: '#F3F9FD',
+    primaryInactive: '#FCB195',
+  },
+};
+
+export const CombinedDefaultTheme = {
+  ...NavigationDefaultTheme,
+  ...theme,
+  colors: {
+    ...NavigationDefaultTheme.colors,
+    ...theme.colors,
+  },
+};
+export const CombinedDarkTheme = {
+  ...NavigationDarkTheme,
+  ...darkTheme,
+  colors: {
+    ...NavigationDarkTheme.colors,
+    ...darkTheme.colors,
+    background: '#F3F9FD',
+  },
+};
+
+export const appColors = {
+  blueprimary: '#0078C1',
+};
+
+export const appFonts = {
+  BARLOW_BD: 'Barlow-Bold',
+  BARLOW_SB: 'Barlow-SemiBold',
+  BARLOW_MD: 'Barlow-Medium',
+  BARLOW_RG: 'Barlow-Regular',
+};
