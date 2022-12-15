c734268af4eb7c52d13e05956943df7baaafbe23
Tomisin Alufogejo <malcolmtomisin@gmail.com>
2022-12-15 17:30:40 +0100

    bluetooth permissions

--- src/screens/dashboard/home/devices/index.tsx
+++ src/screens/dashboard/home/devices/index.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import {View, Text, Image} from 'react-native';
+import React, {useCallback} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  NativeEventEmitter,
+  NativeModules,
+  Platform,
+  EmitterSubscription,
+  ScrollView,
+} from 'react-native';
 import {Button} from 'src/components';
 import {
   normalizeHeight,
@@ -11,9 +20,104 @@ import {
 import styles from 'src/screens/styles';
 import {RadioButton} from 'react-native-paper';
 import {DeviceConnectProps} from 'src/navigators/dashboard/connect/types';
+import BleManager from 'react-native-ble-manager';
+import {check, PERMISSIONS, RESULTS} from 'react-native-permissions';
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 export default function ListDevices({navigation}: DeviceConnectProps) {
   const [value, setValue] = React.useState('');
+  const [list, setList] = React.useState([]);
+  const [isScanning, setIsScanning] = React.useState<boolean>(false);
+  const peripherals = new Map();
+
+  const startScan = () => {
+    BleManager.scan([], 10, true)
+      .then(() => {
+        console.log('Scan is starting');
+        setIsScanning(true);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const handleDiscoverPeripheral = peripheral => {
+    console.log('Got ble peripheral', peripheral);
+    if (!peripheral.name) {
+      peripheral.name = 'NO NAME';
+    }
+    peripherals.set(peripheral.id, peripheral);
+    setList(Array.from(peripherals.values()));
+  };
+
+  const handleStopScan = () => {
+    console.log('Scan is stopped');
+    setIsScanning(false);
+  };
+
+  const handleDisconnectedPeripheral = data => {
+    let peripheral = peripherals.get(data.peripheral);
+    if (peripheral) {
+      peripheral.connected = false;
+      peripherals.set(peripheral.id, peripheral);
+      setList(Array.from(peripherals.values()));
+    }
+    console.log('Disconnected from ' + data.peripheral);
+  };
+
+  const handleUpdateValueForCharacteristic = data => {
+    console.log(
+      'Received data from ' +
+        data.peripheral +
+        ' characteristic ' +
+        data.characteristic,
+      data.value,
+    );
+  };
+
+  const retrieveConnected = () => {
+    BleManager.getConnectedPeripherals([]).then(results => {
+      if (results.length === 0) {
+        console.log('No connected peripherals');
+      }
+      console.log('see results ==>', results);
+      for (var i = 0; i < results.length; i++) {
+        var peripheral = results[i];
+        peripheral.connected = true;
+        peripherals.set(peripheral.id, peripheral);
+        setList(Array.from(peripherals.values()));
+      }
+    });
+  };
+
+  React.useEffect(() => {
+    let listeners: Array<EmitterSubscription> = [];
+    listeners.push(
+      bleManagerEmitter.addListener(
+        'BleManagerDiscoverPeripheral',
+        handleDiscoverPeripheral,
+      ),
+      bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan),
+      bleManagerEmitter.addListener(
+        'BleManagerDisconnectPeripheral',
+        handleDisconnectedPeripheral,
+      ),
+      bleManagerEmitter.addListener(
+        'BleManagerDidUpdateValueForCharacteristic',
+        handleUpdateValueForCharacteristic,
+      ),
+    );
+
+    startScan();
+
+    return () => {
+      for (let i = 0; i < listeners.length; i++) {
+        listeners[i].remove();
+      }
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -21,68 +125,37 @@ export default function ListDevices({navigation}: DeviceConnectProps) {
       <Text style={styles.pale}>
         Pair with your device below. Make sure your device is powered on.
       </Text>
-      <RadioButton.Group
-        onValueChange={value => {
-          setValue(value);
-          navigation.navigate(appRoutes['Bt Status']);
-        }}
-        value={value}>
-        <RadioButton.Item
-          label="Scanned Device 1"
-          value="first"
-          position="leading"
-          style={{
-            backgroundColor: appColors.white,
-            marginVertical: normalize(4),
-            borderRadius: normalize(8),
+      <ScrollView contentContainerStyle={{}}>
+        <RadioButton.Group
+          onValueChange={value => {
+            setValue(value);
+            //navigation.navigate(appRoutes['Bt Status']);
           }}
-          labelStyle={{
-            fontFamily: appFonts.BARLOW_RG,
-            fontSize: normalize(16),
-            lineHeight: normalize(16 * 1.7),
-            color: appColors.shade3,
-          }}
-          color={appColors.blueprimary}
-          uncheckedColor={appColors.border_grey}
-        />
-        <RadioButton.Item
-          label="Scanned Device 2"
-          value="second"
-          position="leading"
-          style={{
-            backgroundColor: appColors.white,
-            marginVertical: normalize(4),
-            borderRadius: normalize(8),
-          }}
-          labelStyle={{
-            fontFamily: appFonts.BARLOW_RG,
-            fontSize: normalize(16),
-            lineHeight: normalize(16 * 1.7),
-            color: appColors.shade3,
-          }}
-          color={appColors.blueprimary}
-          uncheckedColor={appColors.border_grey}
-        />
-        <RadioButton.Item
-          label="Scanned Device 3"
-          value="third"
-          position="leading"
-          style={{
-            backgroundColor: appColors.white,
-            marginVertical: normalize(4),
-            borderRadius: normalize(8),
-          }}
-          labelStyle={{
-            fontFamily: appFonts.BARLOW_RG,
-            fontSize: normalize(16),
-            lineHeight: normalize(16 * 1.7),
-            color: appColors.shade3,
-          }}
-          color={appColors.blueprimary}
-          uncheckedColor={appColors.border_grey}
-        />
-      </RadioButton.Group>
-      <View style={{marginTop: normalizeHeight(100)}}>
+          value={value}>
+          {list.map((v, i) => (
+            <RadioButton.Item
+              key={`${i}`}
+              label={`Scanned Device ${i + 1} ${v}`}
+              value="first"
+              position="leading"
+              style={{
+                backgroundColor: appColors.white,
+                marginVertical: normalize(4),
+                borderRadius: normalize(8),
+              }}
+              labelStyle={{
+                fontFamily: appFonts.BARLOW_RG,
+                fontSize: normalize(16),
+                lineHeight: normalize(16 * 1.7),
+                color: appColors.shade3,
+              }}
+              color={appColors.blueprimary}
+              uncheckedColor={appColors.border_grey}
+            />
+          ))}
+        </RadioButton.Group>
+      </ScrollView>
+      <View style={{paddingTop: normalizeHeight(100)}}>
         <Button
           style={[
             {
@@ -91,6 +164,13 @@ export default function ListDevices({navigation}: DeviceConnectProps) {
             styles.outline_btn,
           ]}
           mode="outlined"
+          onPress={
+            !isScanning
+              ? retrieveConnected
+              : () => {
+                  console.log('still scanning');
+                }
+          }
           labelStyle={{color: appColors.blueprimary}}>
           Scan
         </Button>
