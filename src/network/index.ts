dc7de15bcd309d09b5da0b0cc178c4316bd08678
Keno Mullings <keno@themullingsgroup.com>
2023-01-17 17:07:20 -0500

    - Login now works. - profile ID and hardware ID are now being persisted. - TODO: Add code to pull APP_URL from env. Currently it is hard-coded.

--- src/network/index.ts
+++ src/network/index.ts
@@ -1,5 +1,7 @@
-import {API_URL} from '@env';
+// import {API_URL} from '@env';
 import axios, {AxiosInstance} from 'axios';
+
+const API_URL='https://custom-pneumatic-web-dev.herokuapp.com';
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_URL,
