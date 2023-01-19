82904bf99480ead116f266bb8f29831caeea4994
Keno Mullings <keno@themullingsgroup.com>
2023-01-19 08:49:12 -0500

    Code clean-up.

--- src/network/index.ts
+++ src/network/index.ts
@@ -1,7 +1,7 @@
 // import {API_URL} from '@env';
 import axios, {AxiosInstance} from 'axios';
 
-const API_URL='https://custom-pneumatic-web-dev.herokuapp.com';
+const API_URL = 'https://custom-pneumatic-web-dev.herokuapp.com';
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: API_URL,
