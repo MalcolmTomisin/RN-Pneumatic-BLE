5e4f4c092d5e37c98b6d1acadeb13e4b3bab3830
Tomisin Alufogejo <malcolmtomisin@gmail.com>
2023-01-23 23:19:15 +0100

    refactor: add error handlers to mutation

--- android/app/src/main/java/com/customcomposite/services/CompositeBluetoothService.java
+++ android/app/src/main/java/com/customcomposite/services/CompositeBluetoothService.java
@@ -137,7 +137,7 @@ public class CompositeBluetoothService extends HeadlessJsTaskService {
         PendingIntent pendingIntent = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
             pendingIntent = PendingIntent.getActivity(this,
-                    0, notificationIntent, PendingIntent.FLAG_MUTABLE);
+                    0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
         } else {
             pendingIntent = PendingIntent.getActivity(this,
                     0, notificationIntent, 0);
