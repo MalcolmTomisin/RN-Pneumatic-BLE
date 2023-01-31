b8743036a253e03439d87611ccf94ca02ad2c901
Tomisin Alufogejo <malcolmtomisin@gmail.com>
2023-01-31 14:46:07 +0100

    refactor

--- src/screens/dashboard/home/bt_status/index.tsx
+++ src/screens/dashboard/home/bt_status/index.tsx
@@ -13,11 +13,12 @@ import {
 import Slider from '@react-native-community/slider';
 import {appColors, appFonts, normalize, normalizeHeight} from 'src/config';
 import {StatusScreenProps} from 'src/navigators/dashboard/connect/types';
-import BleManager, {read} from 'react-native-ble-manager';
+import BleManager from 'react-native-ble-manager';
 import database from '@react-native-firebase/database';
 import {Buffer} from '@craftzdog/react-native-buffer';
 import {parseDataPacket, showToast} from 'src/utils';
 import {useAppAuth} from 'src/store';
+import {DB_NODE} from '@env';
 
 const BleManagerModule = NativeModules.BleManager;
 const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
@@ -111,9 +112,9 @@ export default function Bt_status({route, navigation}: StatusScreenProps) {
         BleManager.retrieveServices(peripheralId).then(() => {
           BleManager.write(peripheralId, uuid, characteristic_uuid, cmd)
             .then(() => {
-              database().ref('/write').push({
-                write: 'successful',
-              });
+              // database().ref('/write').push({
+              //   write: 'successful',
+              // });
             })
             .catch(e => {
               database().ref('/write').push({e});
@@ -169,7 +170,9 @@ export default function Bt_status({route, navigation}: StatusScreenProps) {
     //     rawData: currentResult.rawData,
     //   });
     database()
-      .ref(`/${result.current.profileId}-${result.current.hardwareId}`)
+      .ref(
+        `/${DB_NODE}/${result.current.profileId}-${result.current.hardwareId}`,
+      )
       .push({
         macAddress: result.current.macAddress,
         deviceName: result.current.deviceName,
@@ -216,7 +219,7 @@ export default function Bt_status({route, navigation}: StatusScreenProps) {
               const parsedData = parseDataPacket(buffer);
 
               database()
-                .ref('/debug' + Date.now())
+                .ref('/debug/' + Date.now())
                 .set({
                   parsedData,
                   peripheral: currentPeripheral,
